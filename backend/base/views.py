64411f1392736e979b26b45a98af12e3501db69d
Dean Pierrot <dapierrot21@gmail.com>
2021-11-14 01:44:20 -0500

    Serializing data to json format.

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+
+from .serializers import ProductSerializer
+
 
 # Create your views here.
 
@@ -14,14 +19,13 @@ def getRoutes(req):
 
 @api_view(['GET'])
 def getProducts(req):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(req, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
