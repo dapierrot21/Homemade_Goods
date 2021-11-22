761a413f0807dbd398ee64b8ef24008df3e46fe9
Dean Pierrot <dapierrot21@gmail.com>
2021-11-22 02:50:45 -0500

    Cleaning up views and url into different folders and files.

---
+++ backend/base/views/product_views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.models import Product
+from base.products import products
+
+
+from base.serializers import ProductSerializer
+
+from rest_framework import status
+
+
+#####  Products  #####
+
+@api_view(['GET'])
+def getProducts(req):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(req, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,24 +1,19 @@
-from django.contrib.auth.models import User, update_last_login
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-from .products import products
 from django.contrib.auth.models import User
 
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
-
-# Create your views here.
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -29,37 +24,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         data.update(serializer)
 
-        # for key, value in serializer.items():
-        #     data[key] = value
-
         return data
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
-
-#####  Routes  #####
-
-@api_view(['GET'])
-def getRoutes(req):
-    return Response('Hello')
-
-
-#####  Products  #####
-
-@api_view(['GET'])
-def getProducts(req):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(req, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
 
 
 #####  Users  #####
