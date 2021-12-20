8b9e4b016d6b926bd51df4fc38c314a645682a6b
Dean Pierrot <dapierrot21@gmail.com>
2021-12-20 11:34:29 -0500

    Created product list frontend page to view products for admin.

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -1,44 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Table, Button } from 'react-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { listUsers, deleteUser } from '../actions/userActions'
+import { listProducts } from '../actions/productActions'
 
 
-function UserListPage({history}) {
+function ProductListPage({ history, match }) {
     const dispatch = useDispatch()
 
-    const userList = useSelector(state => state.userList)
-    const { loading, error, users } = userList
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
-    const userDelete = useSelector(state => state.userDelete)
-    const { success: successDelete } = userDelete
-
     useEffect(() => {
         if (userInfo && userInfo.isAdmin) {
-            dispatch(listUsers())
+            dispatch(listProducts())
         } else {
             history.push('/login')
         }
         
-    }, [dispatch, history, successDelete, userInfo])
+    }, [dispatch, history, userInfo])
 
 
     const deleteHandler = (id) => {
-        if(window.confirm('Are you sure you want to remove this user?')) {
-            dispatch(deleteUser(id))
+        if(window.confirm('Are you sure you want to remove this product?')) {
+            // Delete products
         }  
+    }
+
+    const createProductHandler = (product) => {
+        //Create product
     }
 
 
     return (
         <div>
-            <h1>Users</h1>
+            <Row className='align-items-center'>
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+
+                <Col className='text-right'>
+                    <Button className='my-3' onClick={createProductHandler}>
+                        <i className='fas fa-plus'></i> Create Product
+                    </Button>
+                </Col>
+            </Row>
+
             {loading 
                 ? (<Loader />)
                 : error
@@ -49,32 +61,29 @@ function UserListPage({history}) {
                                 <tr>
                                     <th>ID</th>
                                     <th>NAME</th>
-                                    <th>EMAIL</th>
-                                    <th>ADMIN</th>
+                                    <th>PRICE</th>
+                                    <th>CATEGORY</th>
+                                    <th>BRAND</th>
                                     <th></th>
                                 </tr>
                             </thead>
 
                             <tbody>
-                                {users.map(user => (
-                                    <tr key={user._id}>
-                                        <td>{user._id}</td>
-                                        <td>{user.name}</td>
-                                        <td>{user.email}</td>
-                                        <td>{user.isAdmin ? (
-                                            <i className='fas fa-check' style={{color:'green'}}></i>
-                                        ) : (
-                                            <i className='fas fa-times' style={{color:'red'}}></i>
-                                        )}</td>
-
+                                {products.map(product => (
+                                    <tr key={product._id}>
+                                        <td>{product._id}</td>
+                                        <td>{product.name}</td>
+                                        <td>${product.price}</td>
+                                        <td>{product.category}</td>
+                                        <td>{product.brand}</td>
                                         <td>
-                                            <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                                            <LinkContainer to={`/admin/product/${product._id}/edit`}>
                                                 <Button variant='light' className='btn-sm'>
                                                     <i className='fas fa-edit'></i>
                                                 </Button>
                                             </LinkContainer>
 
-                                            <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(user._id)}>
+                                            <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
                                                 <i className='fas fa-trash'></i>
                                             </Button>
                                         </td>
@@ -88,4 +97,4 @@ function UserListPage({history}) {
     )
 }
 
-export default UserListPage
+export default ProductListPage
