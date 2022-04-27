f138f9370c1e5165fd7d881303a35f51138c3be2
Dean Pierrot <dapierrot21@gmail.com>
2022-04-27 13:17:03 -0500

    Added font style to submit button

---
+++ backend/frontend/src/components/SearchBox.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+
+function SearchBox() {
+  const [keyword, setKeyword] = useState("");
+
+  let history = useHistory();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword) {
+      history.push(`/?keyword=${keyword}&page=1`);
+    } else {
+      history.push(history.push(history.location.pathname));
+    }
+  };
+
+  return (
+    <Form onSubmit={submitHandler} className="d-flex">
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        className=""
+      ></Form.Control>
+      <Button
+        type="submit"
+        variant="outline-success"
+        className="p-2 navbar-brand"
+      >
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default SearchBox;
