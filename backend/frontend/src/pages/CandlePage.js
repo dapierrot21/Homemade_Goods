355a68c960eaaa1373ee3db3644d1fcf28ec4afd
Dean Pierrot <dapierrot21@gmail.com>
2022-05-03 16:13:05 -0400

    Created Candle product page

--- backend/frontend/src/pages/CandlePage.js
+++ backend/frontend/src/pages/CandlePage.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+import { useDispatch, useSelector } from "react-redux";
+import { Carousel, Col, Row } from "react-bootstrap";
+import { listProducts } from "../actions/productActions";
 
-function CandlePage() {
-  return <div>CandlePage</div>;
+function CandlePage({ history }) {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products, page, pages } = productList;
+
+  let keyword = history.location.search;
+
+  const candles = products.filter(
+    (product) => product.category.toLowerCase() === "candle"
+  );
+
+  useEffect(() => {
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
+
+  return (
+    <div>
+      {!keyword}
+      <h1>Candle Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <div>
+          <Row>
+            {candles.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          {/* <Paginate page={page} pages={pages} keyword={keyword} /> */}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default CandlePage;
