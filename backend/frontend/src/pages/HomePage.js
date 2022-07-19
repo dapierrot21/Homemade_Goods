34b5eac1f1edc6add595b55b8da07bf9a7ffd121
Dean Pierrot <dapierrot21@gmail.com>
2022-07-19 05:06:11 -0400

    updated file

--- backend/frontend/src/pages/HomePage.js
+++ backend/frontend/src/pages/HomePage.js
@@ -1,46 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import Product from '../components/Product'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-import Paginate from '../components/Paginate'
-import ProductCarousel from '../components/ProductCarousel'  
-import { useDispatch, useSelector } from 'react-redux'
-import { Carousel, Col, Row } from 'react-bootstrap'
-import { listProducts } from '../actions/productActions'
+import React, { useState, useEffect } from "react";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+import { useDispatch, useSelector } from "react-redux";
+import { Carousel, Col, Row } from "react-bootstrap";
+import { listProducts } from "../actions/productActions";
 
-function HomePage({history}) {
-    const dispatch = useDispatch()
+function HomePage({ history }) {
+  const dispatch = useDispatch();
 
-    const productList = useSelector(state => state.productList)
-    const { error, loading, products, page, pages} = productList
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products, page, pages } = productList;
 
-    let keyword = history.location.search
+  let keyword = history.location.search;
 
-    useEffect(() => {
-        dispatch(listProducts(keyword))
+  const topSellers = products.filter((product) => product.rating > 4);
 
-    }, [dispatch, keyword])
+  useEffect(() => {
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
-    return (
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {!keyword && <ProductCarousel />}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <div>
-            {!keyword && <ProductCarousel />}
-            <h1>Lastest Products</h1>
-            {loading ? <Loader />
-                : error ? <Message variant='danger'>{error}</Message>
-                : 
-                <div>
-                    <Row>
-                        {products.map(product => (
-                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                <Product product={product} />
-                            </Col>
-                        ))}
-                    </Row>
-                    <Paginate page={page} pages={pages} keyword={keyword}/>
-                </div>
-        }
+          <Row>
+            <h1>Ingredents used</h1>
+          </Row>
+          <Row>
+            <h1>Ingredents used</h1>
+          </Row>
+          <Row>
+            <h1>Ingredents used</h1>
+          </Row>
+          <Row>
+            <h1>Ingredents used</h1>
+          </Row>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
